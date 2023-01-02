972da7e7920f64c1cebf8e2f287d84fa14a4a707
Kua <popoykua28@gmail.com>
2023-01-02 13:46:19 +0800

    Add default in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,83 +1,90 @@
 const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
 
-gulp.task("licenses", async function () {
-  // this is to add Creative Tim licenses in the production mode for the minified js
-  gulp
-    .src("build/static/js/*chunk.js", { base: "./" })
-    .pipe(
-      gap.prependText(`/*!
 
-=========================================================
-* Notus React - v1.1.0 based on Tailwind Starter Kit by Creative Tim
-=========================================================
+function defaultTask() {
 
-* Product Page: https://www.creative-tim.com/product/notus-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/notus-react/blob/main/LICENSE.md)
+  gulp.task("licenses", async function () {
+    // this is to add Creative Tim licenses in the production mode for the minified js
+    gulp
+      .src("build/static/js/*chunk.js", { base: "./" })
+      .pipe(
+        gap.prependText(`/*!
+  
+  =========================================================
+  * Notus React - v1.1.0 based on Tailwind Starter Kit by Creative Tim
+  =========================================================
+  
+  * Product Page: https://www.creative-tim.com/product/notus-react
+  * Copyright 2021 Creative Tim (https://www.creative-tim.com)
+  * Licensed under MIT (https://github.com/creativetimofficial/notus-react/blob/main/LICENSE.md)
+  
+  * Tailwind Starter Kit Page: https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation
+  
+  * Coded by Creative Tim
+  
+  =========================================================
+  
+  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  
+  */`)
+      )
+      .pipe(gulp.dest("./", { overwrite: true }));
 
-* Tailwind Starter Kit Page: https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation
+    // this is to add Creative Tim licenses in the production mode for the minified html
+    gulp
+      .src("build/index.html", { base: "./" })
+      .pipe(
+        gap.prependText(`<!--
+  
+  =========================================================
+  * Notus React - v1.1.0 based on Tailwind Starter Kit by Creative Tim
+  =========================================================
+  
+  * Product Page: https://www.creative-tim.com/product/notus-react
+  * Copyright 2021 Creative Tim (https://www.creative-tim.com)
+  * Licensed under MIT (https://github.com/creativetimofficial/notus-react/blob/main/LICENSE.md)
+  
+  * Tailwind Starter Kit Page: https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation
+  
+  * Coded by Creative Tim
+  
+  =========================================================
+  
+  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  
+  -->`)
+      )
+      .pipe(gulp.dest("./", { overwrite: true }));
 
-* Coded by Creative Tim
+    // this is to add Creative Tim licenses in the production mode for the minified css
+    gulp
+      .src("build/static/css/*chunk.css", { base: "./" })
+      .pipe(
+        gap.prependText(`/*!
+  
+  =========================================================
+  * Notus React - v1.1.0 based on Tailwind Starter Kit by Creative Tim
+  =========================================================
+  
+  * Product Page: https://www.creative-tim.com/product/notus-react
+  * Copyright 2021 Creative Tim (https://www.creative-tim.com)
+  * Licensed under MIT (https://github.com/creativetimofficial/notus-react/blob/main/LICENSE.md)
+  
+  * Tailwind Starter Kit Page: https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation
+  
+  * Coded by Creative Tim
+  
+  =========================================================
+  
+  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  
+  */`)
+      )
+      .pipe(gulp.dest("./", { overwrite: true }));
+    return;
+  });
 
-=========================================================
+}
 
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/`)
-    )
-    .pipe(gulp.dest("./", { overwrite: true }));
-
-  // this is to add Creative Tim licenses in the production mode for the minified html
-  gulp
-    .src("build/index.html", { base: "./" })
-    .pipe(
-      gap.prependText(`<!--
-
-=========================================================
-* Notus React - v1.1.0 based on Tailwind Starter Kit by Creative Tim
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/notus-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/notus-react/blob/main/LICENSE.md)
-
-* Tailwind Starter Kit Page: https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
--->`)
-    )
-    .pipe(gulp.dest("./", { overwrite: true }));
-
-  // this is to add Creative Tim licenses in the production mode for the minified css
-  gulp
-    .src("build/static/css/*chunk.css", { base: "./" })
-    .pipe(
-      gap.prependText(`/*!
-
-=========================================================
-* Notus React - v1.1.0 based on Tailwind Starter Kit by Creative Tim
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/notus-react
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/notus-react/blob/main/LICENSE.md)
-
-* Tailwind Starter Kit Page: https://www.creative-tim.com/learning-lab/tailwind-starter-kit/presentation
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/`)
-    )
-    .pipe(gulp.dest("./", { overwrite: true }));
-  return;
-});
+exports.default = defaultTask
